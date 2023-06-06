624704f55db676fb8432bcdf8b790ba487976303
Candice Duhoux <dhxcancan@gmail.com>
2023-06-06 13:46:34 +0200

    ajout animations

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -53,3 +53,61 @@ if (menuBurger != undefined && menuBurger != null) {
         });
     };
 };
+
+/*REVEAL*/
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+function reveal1() {
+    var reveals1 = document.querySelectorAll(".reveal1");
+
+    for (var i = 0; i < reveals1.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals1[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals1[i].classList.add("active");
+
+        } else {
+            reveals1[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal1);
+
+function reveal2() {
+    var reveals2 = document.querySelectorAll(".reveal2");
+
+    for (var i = 0; i < reveals2.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals2[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals2[i].classList.add("active");
+
+        } else {
+            reveals2[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal2);
